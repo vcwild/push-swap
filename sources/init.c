4fa6a4a31883472e01b2bed580f578b46382eba3
Victor Wildner <vcwildm@gmail.com>
2022-03-02 21:23:58 -0300

    fix: change stack definition

--- sources/init.c
+++ sources/init.c
@@ -3,7 +3,7 @@
 int		init_stack(t_stack *self)
 {
 	self->top = -1;
-	self->vec = (int *)calloc(BUF_MAX_SIZE, sizeof(int));
+	self->vec = (int *)calloc(STACK_MAX_SIZE, sizeof(int));
 	if (!(self->vec))
 		return (1);
 	return (0);
